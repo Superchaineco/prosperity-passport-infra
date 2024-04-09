bedaaae3aa82884ee1b0374da98aea7c310dd3a6
AnotherDev <joseluismanco37@gmail.com>
2024-04-09 08:41:23 -0500

    chore: remove logic from endpoints

--- packages/dpg-account-monolith/src/app/api/get-badges/route.ts
+++ packages/dpg-account-monolith/src/app/api/get-badges/route.ts
@@ -2,28 +2,15 @@ import { badgesService } from '@/services/Badges.service';
 import { createClient } from '@/services/Supabase.service';
 import { NextRequest, NextResponse } from 'next/server';
 
-const supabase = createClient();
-
 export async function GET(req: NextRequest) {
   const eoas = new Headers().get('address')?.split(',');
-  if (!eoas) {
+  const account = new Headers().get('account');
+
+  if (!eoas || !account) {
     return NextResponse.error();
   }
-  await Promise.all(
-    eoas.map(async (address) => {
-      let { data: account, error: accountError } = await supabase
-        .from('Account')
-        .select('*')
-        .eq('address', address)
-        .single();
 
-      if (!account && !accountError) {
-        await supabase.from('Account').insert([{ address }]);
-      }
-    })
-  );
-
-  const currentBadges = await badgesService.getBadges(eoas);
+  const currentBadges = await badgesService.getBadges(eoas, account);
   const totalPoints = currentBadges.reduce((acc, badge) => {
     return acc + badge.points;
   }, 0);
