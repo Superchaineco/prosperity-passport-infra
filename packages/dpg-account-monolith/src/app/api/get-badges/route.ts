9b7a0007112c0fa0f3e42d90ee421449443390b0
AnotherDev <joseluismanco37@gmail.com>
2024-04-09 07:10:49 -0500

    feat: add Account verification

--- packages/dpg-account-monolith/src/app/api/get-badges/route.ts
+++ packages/dpg-account-monolith/src/app/api/get-badges/route.ts
@@ -1,17 +1,34 @@
 import { badgesService } from '@/services/Badges.service';
+import { createClient } from '@/services/Supabase.service';
 import { NextRequest, NextResponse } from 'next/server';
 
+const supabase = createClient();
+
 export async function GET(req: NextRequest) {
-  const eoas = new Headers().get('address');
+  const eoas = new Headers().get('address')?.split(',');
   if (!eoas) {
     return NextResponse.error();
   }
-  const badges = await badgesService.getBadges(eoas?.split(','));
-  const totalPoints = badges.reduce((acc, badge) => {
+  await Promise.all(
+    eoas.map(async (address) => {
+      let { data: account, error: accountError } = await supabase
+        .from('Account')
+        .select('*')
+        .eq('address', address)
+        .single();
+
+      if (!account && !accountError) {
+        await supabase.from('Account').insert([{ address }]);
+      }
+    })
+  );
+
+  const currentBadges = await badgesService.getBadges(eoas);
+  const totalPoints = currentBadges.reduce((acc, badge) => {
     return acc + badge.points;
   }, 0);
   return NextResponse.json({
     totalPoints,
-    badges,
+    currentBadges,
   });
 }
