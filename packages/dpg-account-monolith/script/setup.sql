641603ba9bc8af7dfe6ad326005bdb2bd361c224
AnotherDev <joseluismanco37@gmail.com>
2024-04-09 07:10:30 -0500

    chore: minimal issue

--- packages/dpg-account-monolith/script/setup.sql
+++ packages/dpg-account-monolith/script/setup.sql
@@ -1,10 +1,8 @@
-CREATE TABLE Account (
-   address VARCHAR(42) PRIMARY KEY
-);
-
+CREATE TABLE Account (address VARCHAR(42) PRIMARY KEY);
 CREATE TABLE Badge(
     id SERIAL PRIMARY KEY,
     account VARCHAR(42),
+    title  TEXT not null,
     points INT not null,
     lastClaim timestamp,
     lastClaimBlock INT,
