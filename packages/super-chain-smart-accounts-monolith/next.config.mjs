80d5d52e345edf1563bb766bf1a8197cba92e09a
AnotherDev <joseluismanco37@gmail.com>
2024-04-13 15:50:21 -0500

    feat: add svg webpack config

--- packages/super-chain-smart-accounts-monolith/next.config.mjs
+++ packages/super-chain-smart-accounts-monolith/next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: false,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: { removeViewBox: false },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
