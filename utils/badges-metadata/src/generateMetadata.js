8bef4ff2f6e50525afc8b2cbafadb9352ad04ee6
anotherdev <joseluismanco37@gmail.com>
2025-09-30 21:13:59 -0500

    fix: badges

--- utils/badges-metadata/src/generateMetadata.js
+++ utils/badges-metadata/src/generateMetadata.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const BADGES_FILE_PATH = './badges.json';
 const OUTPUT_FILE = './badges-with-uris.json';
-const IPFS_BASE_URI = 'ipfs/QmSUYPZ2o1dw1EXmRKyxn4JVQ17uPcCToUVqWrMb96Biup';
+const IPFS_BASE_URI = 'ipfs/QmYmpHyybED4mmgQYohYtUEXe2uDxhaAHKyxodBoUBNc1R';
 
 function generateFileName(badgeId, levelId) {
   const paddedBadgeId = badgeId.toString().padStart(64, '0');
